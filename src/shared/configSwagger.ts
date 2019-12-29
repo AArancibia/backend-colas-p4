d342a7af08ba467e8d10c46edd60738345c2a5c4
Alexis Arancibia <alexis2396@hotmail.com>
2019-12-28 22:52:10 -0500

    fix environment

--- src/shared/configSwagger.ts
+++ src/shared/configSwagger.ts
@@ -1,17 +1,25 @@
 import { DocumentBuilder } from '@nestjs/swagger';
-import * as dotenv from 'dotenv';
+//import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 
+const dotenv = require('dotenv');
 const environment = process.env.NODE_ENV;
-const data = dotenv.parse( fs.readFileSync(`${environment}.env`) );
-const PORT = data.PORT;
+
+dotenv.config({
+  path: `${environment}.env`,
+});
+
+//const data = dotenv.parse( fs.readFileSync(`${environment}.env`) );
+const PORT = process.env.PORT;
 
 export const swaggerBaseConfig = new DocumentBuilder()
-  .setTitle( 'API REST - Proyecto Colas - Municipalidad de Villa El Salvador' )
-  .setDescription( 'Aquí se encuentran todos los servicios del proyecto de Colas de la MuniVES' )
-  .setVersion( '1.0.0' )
-  .setHost(  String( `http://localhost:${ PORT }` ).split('//')[1] )
-  .setBasePath( '/api' )
+  .setTitle('API REST - Proyecto Colas - Municipalidad de Villa El Salvador')
+  .setDescription(
+    'Aquí se encuentran todos los servicios del proyecto de Colas de la MuniVES',
+  )
+  .setVersion('1.0.0')
+  .setHost(String(`http://localhost:${PORT}`).split('//')[1])
+  .setBasePath('/api')
   //.addTag( 'Colas', 'Proyecto' )
   //.addTag( 'NestJS', 'Framework')
   .build();
